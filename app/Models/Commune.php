838dbdc5b0160dce42b4b956079f9237ba6db508
Teguera <aboubacarteguera21@example.com>
2024-02-12 15:13:49 +0000

    relation entre housingPost et commune

--- app/Models/Commune.php
+++ app/Models/Commune.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Commune extends Model
 {
@@ -14,6 +15,11 @@ class Commune extends Model
     public function city():BelongsTo
     {
         return $this->belongsTo(City::class);
+    }
+
+    public function housingPost():HasMany
+    {
+        return $this->hasMany(HousingPost::class);
     }
 
     public function searchPosts():BelongsToMany
--- app/Models/HousingPost.php
+++ app/Models/HousingPost.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class HousingPost extends Model
 {
     use HasFactory;
+
+    public function commune():BelongsTo
+    {
+        return $this->belongsTo(Commune::class);
+    }
 }
