898ccafc109b654e39bd5dc190495f7ff284e828
Teguera <aboubacarteguera21@example.com>
2024-02-12 14:58:45 +0000

    relation entre city et commune

--- app/Models/HousingPost.php
+++ app/Models/HousingPost.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SearchPost extends Model
+class HousingPost extends Model
 {
     use HasFactory;
-
 }
--- database/migrations/2024_01_31_100502_create_housing_types_table.php
+++ database/migrations/2024_01_31_100502_create_housing_types_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('housing_type', function (Blueprint $table) {
+        Schema::create('housing_types', function (Blueprint $table) {
             $table->id();
             $table->string('libelle');
             $table->timestamps();
